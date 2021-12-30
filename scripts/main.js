794e01b7c58f49ea70e3f65ebd2ef593a449136c
DimaGT <dimon200117@gmail.com>
2021-12-30 21:49:56 +0200

    added shadow

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,15 @@
 const board = document.getElementById("board");
 const colors = [
-  "red",
-  "blue",
-  "yellow",
+  "#e74c3c",
+  "#8e44ad",
   "purple",
   "cyan",
-  "orange",
+  "#3498db",
   "pink",
-  "gold",
+  "e67e22",
+  "#2ecc71",
 ];
-const SQUARES_NUMBER = 500;
+const SQUARES_NUMBER = 1394;
 
 for (let i = 0; i < SQUARES_NUMBER; i++) {
   const square = document.createElement("div");
@@ -28,10 +28,12 @@ for (let i = 0; i < SQUARES_NUMBER; i++) {
 const setColor = (element) => {
   const color = getRandomColor();
   element.style.backgroundColor = color;
+  element.style.boxShadow = `0 0 10px ${color}`;
 };
 
 const removeColor = (element) => {
   element.style.backgroundColor = "#1d1d1d";
+  element.style.boxShadow = `0 0 2px #1d1d1d`;
 };
 
 const getRandomColor = () => {
